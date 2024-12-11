8f2c2bd87a6eb7dfe5db933da690574f8daa4079
fatimanz2712 <fatimanazemi2712@gmail.com>
2024-12-11 23:19:42 +0100

    Initial Commit

---
+++ Mainpro/MainproApp.swift
@@ -0,0 +1,17 @@
+//
+//  MainproApp.swift
+//  Mainpro
+//
+//  Created by fatima nazemi on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MainproApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
